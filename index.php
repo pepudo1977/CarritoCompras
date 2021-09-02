0961d74612f605836a4669a3c6769401f5839987
pepudo1977 <89487875+pepudo1977@users.noreply.github.com>
2021-09-01 20:49:10 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -28,7 +28,6 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<link rel="stylesheet" type="text/css" href="estilo.css">	
 	<title>Formulario</title>
 	<!-- Latest compiled and minified CSS -->	
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
@@ -38,25 +37,30 @@
 
 	<!-- Latest compiled and minified JavaScript -->
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+	<link rel="stylesheet" type="text/css" href="estilo.css">	
+
 </head>
 <body>
 
-	<form method="post"  >
+	<form action="registar.php" method="post"  >
 		<fieldset>
-			<div class="container">
-			<div class="panel panel-default"> 
+			<div class="container" id="formulario">
+			<div class="panel panel-primary"> 
 			<div class="panel-heading text-center">
 			<legend>Formulario</legend>
 			<input class="controls" type="text" name="cedula" id="cedula" placeholder="Ingrese su cedula">
 			<input class="controls" type="text" name="nombre" id="nombre" placeholder="Ingrese su nombre">	
-			<input class="controls"	type="text" name="apellido"id="apellido" placeholder="Ingrese su apellido">		
-			<input class="controls"	type="text" name="direccion"id="direccion" placeholder="Ingrese su direccion">		
-			<input class="controls"	type="text" name="telefono"id="telefono" placeholder="Ingrese su telefono">			
+			<input class="controls"	type="text" name="direccion"id="apellido" placeholder="Ingrese su direccion">	
+			<input class="controls"	type="text" name="telefono"id="direccion" placeholder="Ingrese su telefono">		
+			<input class="controls"	type="text" name="clave"id="clave" placeholder="Ingrese su clave">	
+			<div class="panel-heading">		
 		<p> Estoy de acuerdo con <a href="#"> Terminos y condiciones</a> </p>
-		<input class="botons" type="sbmit" value="Registrar">
+		<button class="btn btn-success"> registar</button>
+		<button class="btn btn-info"> verificar</button>
+		
 		<p><a href="#"> ¿ya tengo cuenta</a></p>
 			
-				
+			</div>
 			</div>
 			</div> 
 			</div>
